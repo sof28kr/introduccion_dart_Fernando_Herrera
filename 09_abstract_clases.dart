fd6243eaa8ad183247dc4170b19db18818d1ade4
sof28kr <156102526+sof28kr@users.noreply.github.com>
2024-02-06 19:17:55 -0500

    abstract clases power plants

--- 09_abstract_clases.dart
+++ 09_abstract_clases.dart
@@ -1,12 +1,24 @@
 void main (){
   
-  // final windPlant = EnergyPlant();
+  final windPlant = WindPlant(initialEnergy:100);
+  
+  print (windPlant);
+  print ('wind : ${chargePhone(windPlant)}');
   
 }
 
 // las clases abstarctas nos pueden ser inicializadas
 // algo como
 // final windPlant = EnergyPlant(); 
+
+
+double chargePhone(EnergyPlant plant){
+  if (plant.energyLeft<10) {
+    throw Exception ('Not enought energy');
+  }
+  return plant.energyLeft -10;
+}
+
 
 
 //creamos una lista ya que tenemos valores en 
@@ -26,5 +38,19 @@ abstract class EnergyPlant {
   
   void consumeEnergy( double amount);
   
-  
 }
+
+// extends vs implements
+class WindPlant extends EnergyPlant {
+  WindPlant ({required double initialEnergy})
+    : super(energyLeft: initialEnergy, type: PlantType.eolica);
+
+  @override
+  void consumeEnergy(double amount) {
+    energyLeft -= amount; 
+  }
+
+}
+
+
+
