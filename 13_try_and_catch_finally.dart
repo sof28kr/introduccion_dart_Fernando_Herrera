2d62bfb799800624fc34d79ba0e6d81951eb4927
sof28kr <156102526+sof28kr@users.noreply.github.com>
2024-02-09 17:49:51 -0500

    Update 13_try_and_catch_finally.dart

--- 13_try_and_catch_finally.dart
+++ 13_try_and_catch_finally.dart
@@ -1,1 +1,29 @@
+void main() async {
+  
+  print('inicio del programa');
+  
+  // El try catch nos deja intentar y tener una alternativa a si todo falla
+  try {
+    final value = await httpGet('http://fernando-herrera.com/cursos');
+    print('exito: $value');
+    
+  } on Exception catch(err) {
+    print('tenemos una Exception: $err');
+    
+  } finally {
+    print('Fin del try y catch');
+  }
+    
+  print('fin del programa');
+}
 
+// un asyncrono siempre va devolver un future
+
+Future <String> httpGet (String url) async {
+  
+  return Future.delayed( const Duration(seconds: 1), (){
+  
+  throw Exception('No hay parametros en le URL') ;
+    
+  });
+}
