b6144661308cc2907228e7581c88eff7f9fc804b
sof28kr <156102526+sof28kr@users.noreply.github.com>
2024-02-09 17:06:13 -0500

    xD

--- 11_futures.dart
+++ 11_futures.dart
@@ -1,25 +1,25 @@
-void main() async {
-  
+void main() {
   print('inicio del programa');
   
-  // El try catch nos deja intentar y tener una alternativa a si todo falla
-  try {
-    final value = await httpGet('http://fernando-herrera.com/cursos');
-    print(value);
-  } catch (err){
-    print("tenemos un error: ");
-  }
+  httpGet('http://fernando-herrera.com/cursos').then(
+    (value) {
+      print( value );
+    })
+    .catchError((err) {
+    print('Error: $err');
+  });
   
   print('fin del programa');
 }
 
-// un asyncrono siempre va devolver un future
-
-Future <String> httpGet (String url) async {
+Future <String> httpGet (String url) {
   
   return Future.delayed( const Duration(seconds: 1), (){
-  
-  throw "Error en la peticion";
     
+    // throw es una excepcion, es dcir lo que pasa cuando
+    throw 'Error en la peticion http';
+    
+    // un futuro nunca llega
+    return 'Respuesta de la peticion http';
   });
 }
